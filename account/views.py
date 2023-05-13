7d0e2d58ee7e5054fc83679099b53d6890891bcd
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-13 10:03:00 +0500

    Registration Preparation: Setting Up urls and Design

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from . forms import RegistrationForm
 # Create your views here.
+
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    pass
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('account/', include('account.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
