9c82bb36007dd5edf05aaf2ef576a4d2da3ab862
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-13 10:32:38 +0500

    implementing model forms and editing init method

--- account/forms.py
+++ account/forms.py
@@ -15,3 +15,12 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name',
                   'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
