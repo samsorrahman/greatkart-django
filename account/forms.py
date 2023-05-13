af4e2b70201ec09d52fc839aa82b987c531028e4
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-13 10:24:58 +0500

    transformed the form to django forms

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,14 @@ from . models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
+
     class Meta:
         model = Account
         fields = ['first_name', 'last_name',
