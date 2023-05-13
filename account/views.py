6784dc7be9d71ff8f1f26986b7d8ca80c20d367d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-13 22:14:41 +0500

    Login Credential function updated

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from . forms import RegistrationForm
 from .models import Account
-from django.contrib import messages
+from django.contrib import messages, auth
 # Create your views here.
 
 
@@ -31,6 +31,19 @@ def register(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid Login Credentials')
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
 
