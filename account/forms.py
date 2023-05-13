50f1b93e168a3689c3c747054916ef53894ce2f0
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-13 11:08:24 +0500

    Making view and Editing model form clean method to check passwords

--- account/forms.py
+++ account/forms.py
@@ -16,6 +16,16 @@ class RegistrationForm(forms.ModelForm):
         fields = ['first_name', 'last_name',
                   'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
