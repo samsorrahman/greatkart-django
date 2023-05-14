74febf986afa5bac231a8cc87208a86bad39d1a9
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-14 17:16:04 +0500

    forgot password function completed

--- account/views.py
+++ account/views.py
@@ -27,7 +27,7 @@ def register(request):
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-
+            # user activation
             current_site = get_current_site(request)
             mail_subject = 'Please activate your account'
             message = render_to_string('accounts/account_verification_email.html', {
@@ -112,5 +112,25 @@ def forgotPassword(request):
         email = request.POST['email']
         if Account.objects.filter(email=email).exists():
             user = Account.objects.get(email__exact=email)
+
+            # reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.error(
+                request, 'Password reset email has been sent to your email address')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist')
+            return redirect('forgotPassword')
 
     return render(request, 'accounts/forgotPassword.html')
