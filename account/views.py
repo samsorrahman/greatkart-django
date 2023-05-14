17285ca61614c3201386a3621dcdf735f2b4e924
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-14 05:59:17 +0500

    started working on user verification

--- account/views.py
+++ account/views.py
@@ -3,7 +3,13 @@ from django.contrib.auth.decorators import login_required
 from . forms import RegistrationForm
 from .models import Account
 from django.contrib import messages, auth
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
 # Create your views here.
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 
 def register(request):
@@ -20,6 +26,19 @@ def register(request):
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
             messages.success(request, "Registration Successfull!")
             return redirect('register')
     else:
