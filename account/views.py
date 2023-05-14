0e7f20dbd0bdbae7b0d65446db7172af9f0dc58a
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-14 08:22:38 +0500

    account activation  Decode user pk and activate the user | Expire link

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from . forms import RegistrationForm
 from .models import Account
@@ -39,8 +40,9 @@ def register(request):
             send_email = EmailMessage(mail_subject, message, to=[to_email])
             send_email.send()
 
-            messages.success(request, "Registration Successfull!")
-            return redirect('register')
+            # messages.success(
+            #     request, "Thank you for registration please activate you account!")
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -71,3 +73,21 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(
+            request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
