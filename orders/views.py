3f95a662652b0cf3451df1e905b2e4671f997bc0
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-16 03:42:49 +0500

    Place new order view and generate order number for port

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from carts.models import CartItem
 # Create your views here.
+
+
+def place_order(request):
+    current_user = request.user
+
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect('store')
+
+    return HttpResponse('ok')
