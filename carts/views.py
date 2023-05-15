775945fb9c6a0e70ee74ec4ec0a50f1ea06f5d63
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-15 23:24:42 +0500

    modify cart counter/ cart view to handle logged in user

--- carts/views.py
+++ carts/views.py
@@ -106,13 +106,21 @@ def remove_cart_item(request, product_id, cart_item_id):
 
 def cart(request, total=0, quantity=0, cart_item=None):
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        tax = 0
+        grand_total = 0
+
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(
+                user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = tax+total
+
     except:
         pass
 
