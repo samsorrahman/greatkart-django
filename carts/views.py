811a23bb4ed184bc7e3771334cc46c20943b262d
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-15 21:18:24 +0500

    checkout page started

--- carts/views.py
+++ carts/views.py
@@ -124,3 +124,7 @@ def cart(request, total=0, quantity=0, cart_item=None):
     }
 
     return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    return render(request, 'store/checkout.html')
