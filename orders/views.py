67a84fdc2407a02c7139fe47665f2cffa73900a7
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-17 23:56:16 +0500

    function updated

--- orders/views.py
+++ orders/views.py
@@ -6,13 +6,21 @@ from . models import Order
 # Create your views here.
 
 
-def place_order(request):
+def place_order(request, total=0, quantity=0):
     current_user = request.user
 
     cart_items = CartItem.objects.filter(user=current_user)
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('store')
+    grand_total = 0
+    tax = 0
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)
+        quantity += cart_item.quantity
+    tax = (2 * total)/100
+    grand_total = tax+total
 
     if request.method == 'POST':
         form = OrderForm(request.POST)
@@ -29,3 +37,7 @@ def place_order(request):
             data.state = form.cleaned_data('state')
             data.city = form.cleaned_data('city')
             data.order_note = form.cleaned_data('order_note')
+            data.order_total = grand_total
+            data.tax = tax
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
