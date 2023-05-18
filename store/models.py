1567783307fd7e4fce84c3deba0dff589aa26432
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-18 23:46:06 +0500

    ReviewRating Model created

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from Catagory.models import Category
+from account.models import Account
 # Create your models here.
 
 
@@ -49,3 +50,18 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+
+
+class ReviewRating(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100, blank=True)
+    review = models.TextField(max_length=500, blank=True)
+    rating = models.FloatField()
+    ip = models.CharField(max_length=20, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.subject
