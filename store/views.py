2e38e2498a3e1fee8080ecc6fbb21799fa51fb8f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-19 01:19:51 +0500

    Review function created

--- store/views.py
+++ store/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 
 from carts.models import CartItem
-from .models import Product
+from .models import Product, ReviewRating
 from Catagory.models import Category
 # Create your views here.
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
+from .forms import ReviewForm
 
 
 def store(request, catagory_slug=None):
@@ -69,5 +71,28 @@ def search(request):
     return render(request, 'store/store.html', context)
 
 
-def submit_review(request):
-    return
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(
+                user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(
+                request, 'Thank you! Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(
+                    request, 'Thank you! Your review has been submitted.')
+                return redirect(url)
